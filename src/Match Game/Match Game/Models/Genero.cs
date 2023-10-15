0193cf51a4326059cea605724e69f0e1a39eac3a
AndreeGS <andre.guilherme2009@gmail.com>
2023-10-15 10:27:13 -0300

    Reset pt2

---
+++ src/Match Game/Match Game/Models/Genero.cs
@@ -0,0 +1,9 @@
+﻿namespace Match_Game.Models
+{
+    public class Genero
+    {
+        public string Nome_Genero { get; set; }
+        public int ID_Genero { get; set; }
+
+    }
+}
---
+++ src/Match Game/Match Game/Models/Jogo.cs
@@ -0,0 +1,10 @@
+﻿namespace Match_Game.Models
+{
+    public class Jogo
+    {
+        public string Nome { get; set; }
+        public int ID_Jogo { get; set; }
+
+        public string Descricao { get; set; }
+    }
+}
