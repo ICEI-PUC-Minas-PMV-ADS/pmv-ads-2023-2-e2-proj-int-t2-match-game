1f31f31e3de995f67b5839a83bb9673fb757aa55
AndreeGS <andre.guilherme2009@gmail.com>
2023-10-15 10:21:24 -0300

    Reset

--- src/Match Game - Perfil/Match Game - Perfil/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Match_Game___Perfil.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- src/Match Game - Perfil/Match Game - Perfil/Models/UserName.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Match_Game___Perfil.Models
-{
-    public class UserName
-    {
-        public string Name { get; set; }
-    }
-}
