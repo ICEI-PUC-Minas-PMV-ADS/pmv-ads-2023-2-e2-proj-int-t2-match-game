0dfb8c8eb1daa3586f39494373492861a48c7bad
AndreeGS <andre.guilherme2009@gmail.com>
2023-10-17 21:47:42 -0300

    Restrição da string conexão

---
+++ src/Match Game - Perfil/Match Game - Perfil/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Match_Game___Perfil.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
