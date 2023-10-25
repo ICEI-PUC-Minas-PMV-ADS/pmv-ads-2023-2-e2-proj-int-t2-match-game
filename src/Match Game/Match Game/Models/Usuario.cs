39210b65b72e151303600659f2fceb387747234b
Luana Pina <luanagpina.mkt@gmail.com>
2023-10-24 22:04:17 -0300

    Desenvolvimento do front-end da área de cadastro de usuários

--- src/Match Game/Match Game/Models/Usuario.cs
+++ src/Match Game/Match Game/Models/Usuario.cs
@@ -3,6 +3,7 @@
     public class Usuario
     {
         public string Nome { get; set; }
+
         public DateTime Data_Nascimento { get; set; }
         public string Email { get; set; }
         public string Senha { get; set; }
