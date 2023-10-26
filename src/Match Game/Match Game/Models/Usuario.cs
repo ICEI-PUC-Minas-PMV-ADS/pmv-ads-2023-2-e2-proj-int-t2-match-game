228e7aa477d4815dec288c600dfcc1b5b2ba7e94
Luana Pina <luanagpina.mkt@gmail.com>
2023-10-26 20:52:09 -0300

    Implementação footer global, implementação quase finalizada do header, fix de um erro na área de cadastro.

--- src/Match Game/Match Game/Models/Usuario.cs
+++ src/Match Game/Match Game/Models/Usuario.cs
@@ -1,10 +1,14 @@
-﻿namespace Match_Game.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Match_Game.Models
 {
     public class Usuario
     {
         public string Nome { get; set; }
 
+        [DataType(DataType.Date)]
         public DateTime Data_Nascimento { get; set; }
+
         public string Email { get; set; }
         public string Senha { get; set; }
 
