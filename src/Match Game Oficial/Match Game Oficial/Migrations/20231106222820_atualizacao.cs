cd7f783ba2223a982420cf21fd4dd931983c0d83
Bruna Santos <s_bruna@live.com>
2023-11-06 19:38:32 -0300

    atualizações

---
+++ src/Match Game Oficial/Match Game Oficial/Migrations/20231106222820_atualizacao.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Match_Game_Oficial.Migrations
+{
+    public partial class atualizacao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
