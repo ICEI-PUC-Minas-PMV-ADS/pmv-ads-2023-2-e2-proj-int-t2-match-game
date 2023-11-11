1f6ee0c0f5c0b548f6f3a2c31417525cdfcde0cc
AndreeGS <andre.guilherme2009@gmail.com>
2023-11-11 11:32:33 -0300

    AT-Favoritos

---
+++ src/Match Game Oficial/Match Game Oficial/Controllers/FavoritosController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace Match_Game_Oficial.Controllers
+{
+    public class FavoritosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    
+    
+    }
+}
